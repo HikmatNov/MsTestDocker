fc79d0eb7d3b0c63c377e602c4265beaa7ca24d9
HikmatNov <hikmatnovruzaliyev@gmail.com>
2024-01-05 18:07:03 +0400

    Initial commit

---
+++ src/main/java/entity/java/User.java
@@ -0,0 +1,21 @@
+package entity.java;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@Table(name="users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String userName;
+    String repeatPassword;
+    String mail;
+
+
+}
